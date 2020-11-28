6ba670411171a1df1506ffb6f2a4f0c3b847e7f9
ShowMaker47 <74054710+ShowMaker47@users.noreply.github.com>
2020-11-28 21:20:04 +0800

    Add files via upload

---
+++ LinkQueue.cpp
@@ -0,0 +1,69 @@
+﻿
+// 链表实现队列Queue的顺序存储结构
+//初始化队列中有一个节点（作为标志节点），当front和rear同时指向该节点时，视为队列为空
+
+#include <iostream>
+typedef int Status;
+#define MAXSIZE 20
+#define OK 1
+#define ERROR -1
+typedef int ElemType;
+
+typedef struct QNode
+{
+    ElemType data;
+	struct QNode *next;
+}QNode,*QueuePtr;
+
+typedef struct {
+    QueuePtr front, rear;
+    int length;
+}LinkQueue;
+
+LinkQueue* initQueue() {
+    //LinkQueue*queue;
+    LinkQueue* queue = (LinkQueue*)malloc(sizeof(LinkQueue));
+    QueuePtr p = (QueuePtr)malloc(sizeof(QNode));
+    p->next = NULL;
+    queue->length = 0;
+    queue->front = p;
+    queue->rear = p;
+    return queue;
+}
+//在链表末尾插入元素
+Status insertElem(LinkQueue* queue, ElemType e) {
+    QNode* s = (QNode*)malloc(sizeof(QNode));
+    s->data = e;
+    s->next = NULL;
+    queue->rear->next = s;
+    queue->rear = s;
+    queue->length++;
+    return OK;
+}
+//从链表头删除元素
+Status deleteElem(LinkQueue* queue, ElemType* e) {
+    if (queue->front == queue->rear) {
+        //队列为空
+        return ERROR;
+    }
+    QueuePtr p = queue->front->next;
+    *e = p->data;
+    queue->front->next= p->next;
+    if (queue->rear == p) {
+        queue->rear = queue->front;
+    }
+    free(p);
+    queue->length--;
+    return OK;
+}
+
+int main()
+{
+    int i = 0;
+    int* e = &i;
+    LinkQueue* queue = initQueue();
+    insertElem(queue, 5);
+    insertElem(queue, 26);
+
+    deleteElem(queue, e);
+}
---
+++ SqQueue.cpp
@@ -0,0 +1,88 @@
+﻿
+// 数组实现队列Queue的顺序存储结构
+
+#include <iostream>
+typedef int Status;
+#define MAXSIZE 20
+#define OK 1
+#define ERROR -1
+typedef int ElemType;
+
+typedef struct 
+{
+    ElemType data[MAXSIZE];
+    int front;  //指向队首
+    int rear;   //指向队尾，指向最后一个元素的下一位
+}SqQueue;
+
+//初始化队列
+SqQueue* initQueue() {
+    SqQueue* queue;
+    queue = (SqQueue*)malloc(sizeof(SqQueue));//分配空间
+    queue->front = 0;//指向队首的指针
+    queue->rear = 0;
+    
+    return queue;
+}
+int QueueLength(SqQueue* queue) {
+    return (queue->rear - queue->front + MAXSIZE) % MAXSIZE;
+}
+//根据元素查找，返回顺序
+
+//在队尾插入元素
+Status insertElem(ElemType X, SqQueue* queue) {
+    //判断是否满
+    if ((queue->rear + 1) % MAXSIZE == queue->front) {
+        return ERROR;
+    }
+    queue->data[queue->rear] = X;
+    if (queue->rear == MAXSIZE - 1) {
+        queue->rear = 0;
+    }
+    else {
+        queue->rear++;
+    }
+    return OK;
+
+}
+//在队首删除元素
+Status deleteElem(SqQueue* queue,ElemType* e) {
+    //判断队列空
+    if (queue->front == queue->rear) {
+        return ERROR;
+    }
+    *e = queue->data[queue->front];
+    if (queue->front == MAXSIZE - 1) {
+        queue->front = 0;
+    }
+    else {
+        queue->front++;
+    }
+    return OK;
+}
+//输出队列序列
+void printQueue(SqQueue* queue) {
+    int k;
+    printf("数组队列共有%d个元素\n", QueueLength(queue));
+    k = queue->front % MAXSIZE;
+    for (int i = 0; i < QueueLength(queue); i++) {
+        printf("第%d个元素的值为%d\n", i+1, queue->data[k]);
+        k++;
+        k = k % MAXSIZE;
+    }
+}
+int main()
+{
+    int a = 0;
+    int* e = &a;
+    SqQueue* Q;  
+    Q=initQueue();
+    insertElem(1, Q);
+    insertElem(2, Q);
+    insertElem(3, Q);
+    insertElem(4, Q);
+    printQueue(Q);
+    deleteElem(Q,e);
+    printQueue(Q);
+}
+
